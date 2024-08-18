47e0426bdec7e0a0027d80632cf4dc0c0ad88325
Thomas Greimel <thomgreimel@gmail.com>
2024-08-18 20:13:17 +0200

    Improved authenticator and added cli program

--- GDJ.Auth/GDJAuthenticator.cs
+++ GDJ.Auth/GDJAuthenticator.cs
@@ -1,6 +1,7 @@
 ﻿using Newtonsoft.Json;
 using SpotifyAPI.Web;
 using SpotifyAPI.Web.Auth;
+using System.ComponentModel;
 using static SpotifyAPI.Web.Scopes;
 
 namespace GDJ.Auth
@@ -9,117 +10,76 @@ namespace GDJ.Auth
     {
 
         private string clientId;
-        private string accessToken;
         private string credentialsPath;
-        private SemaphoreSlim semaphore;
 
         private EmbedIOAuthServer server;
-        private bool serverStarded;
 
-        public GDJAuthenticator(string clientId)
+        public GDJAuthenticator(string clientId, string credentialsPath = "credentials.json")
         {
             this.clientId = clientId;
-            this.credentialsPath = "credentials.json";
-            this.serverStarded = false;
-            this.semaphore = new SemaphoreSlim(0, 1);
+            this.credentialsPath = credentialsPath;
 
             server = new EmbedIOAuthServer(GetRedirectUrl(), 5543);
         }
 
-        public async Task<string> GetAuthorizationToken()
+        public async Task<SpotifyClient> GetSpotifyClientAsync()
         {
-            if (!serverStarded)
-                await server.Start();
 
-            if (File.Exists(credentialsPath))
+            PKCEAuthenticator? authenticator;
+
+            if (!File.Exists(credentialsPath))
             {
-                await Start();
+                var result = new TaskCompletionSource<PKCEAuthenticator>();
+                var (verifier, challenge) = PKCEUtil.GenerateCodes();
+
+                await server.Start();
+                server.AuthorizationCodeReceived += async (sender, response) =>
+                {
+                    await server.Stop();
+                    var token = await new OAuthClient().RequestToken(
+                      new PKCETokenRequest(clientId!, response.Code, server.BaseUri, verifier)
+                    );
+
+                    await File.WriteAllTextAsync(credentialsPath, JsonConvert.SerializeObject(token));
+
+                    var auth = new PKCEAuthenticator(clientId, token);
+                    result.SetResult(auth);
+                };
+
+                var request = new LoginRequest(server.BaseUri, clientId!, LoginRequest.ResponseType.Code)
+                {
+                    CodeChallenge = challenge,
+                    CodeChallengeMethod = "S256",
+                    Scope = new List<string> { UserReadEmail, UserReadPrivate, UserReadPlaybackState, PlaylistReadPrivate, PlaylistReadCollaborative }
+                };
+
+                var uri = request.ToUri();
+                try
+                {
+                    BrowserUtil.Open(uri);
+                }
+                catch (Exception)
+                {
+                    Console.WriteLine("Unable to open URL, manually open: {0}", uri);
+                }
+
+                authenticator = await result.Task;
             } 
             else
             {
-                await StartAuthentication();
+                var json = await File.ReadAllTextAsync(credentialsPath);
+                var token = JsonConvert.DeserializeObject<PKCETokenResponse>(json);
+
+                authenticator = new PKCEAuthenticator(clientId!, token!);
             }
 
-            return accessToken;
-        }
-
-        public async Task<SpotifyClient> GetSpotifyClientAsync()
-        {
-            if (!serverStarded)
-                await server.Start();
-
-            if (File.Exists(credentialsPath))
-            {
-                await Start();
-            }
-            else
-            {
-                await StartAuthentication();
-            }
-
-            return new SpotifyClient(accessToken);
-        }
-
-        private async Task Start()
-        {
-            var json = await File.ReadAllTextAsync(credentialsPath);
-            var token = JsonConvert.DeserializeObject<PKCETokenResponse>(json);
-
-            var authenticator = new PKCEAuthenticator(clientId!, token!);
             authenticator.TokenRefreshed += (sender, token) => File.WriteAllText(credentialsPath, JsonConvert.SerializeObject(token));
-
             var config = SpotifyClientConfig.CreateDefault()
               .WithAuthenticator(authenticator);
 
-            accessToken = token.AccessToken;
-
-            var spotify = new SpotifyClient(config);
-
-            var me = await spotify.UserProfile.Current();
-            Console.WriteLine($"Welcome {me.DisplayName} ({me.Id}), you're authenticated!");
-
-            var playlists = await spotify.PaginateAll(await spotify.Playlists.CurrentUsers().ConfigureAwait(false));
-            Console.WriteLine($"Total Playlists in your Account: {playlists.Count}");
-
-            if (semaphore.CurrentCount == 0)
-                semaphore.Release();
-
             server.Dispose();
-        }
 
-        private async Task StartAuthentication()
-        {
-            var (verifier, challenge) = PKCEUtil.GenerateCodes();
-
-            await server.Start();
-            server.AuthorizationCodeReceived += async (sender, response) =>
-            {
-                await server.Stop();
-                var token = await new OAuthClient().RequestToken(
-                  new PKCETokenRequest(clientId!, response.Code, server.BaseUri, verifier)
-                );
-
-                await File.WriteAllTextAsync(credentialsPath, JsonConvert.SerializeObject(token));
-                await Start();
-            };
-
-            var request = new LoginRequest(server.BaseUri, clientId!, LoginRequest.ResponseType.Code)
-            {
-                CodeChallenge = challenge,
-                CodeChallengeMethod = "S256",
-                Scope = new List<string> { UserReadEmail, UserReadPrivate, UserReadPlaybackState, PlaylistReadPrivate, PlaylistReadCollaborative }
-            };
-
-            var uri = request.ToUri();
-            try
-            {
-                BrowserUtil.Open(uri);
-                await semaphore.WaitAsync();
-            }
-            catch (Exception)
-            {
-                Console.WriteLine("Unable to open URL, manually open: {0}", uri);
-            }
+            return new SpotifyClient(config);
         }
 
         private async Task OnErrorReceived(object sender, string error, string state)
---
+++ GDJ.CLI/Program.cs
@@ -0,0 +1,16 @@
+﻿using GDJ.Auth;
+using SpotifyAPI.Web;
+using SpotifyAPI.Web.Auth;
+
+var clientId = Environment.GetEnvironmentVariable("GDJ_SPOTIFY_CLIENT_ID");
+
+GDJAuthenticator auth = new GDJAuthenticator(clientId);
+//var accessToken = await auth.GetAuthorizationToken();
+
+var spotifyClient = await auth.GetSpotifyClientAsync();
+
+var me = await spotifyClient.UserProfile.Current();
+Console.WriteLine($"Hello, {me.DisplayName}");
+
+var result = await spotifyClient.Playlists.GetUsers(me.Id);
+Console.WriteLine(result.Total);
